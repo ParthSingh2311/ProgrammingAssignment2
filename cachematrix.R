ab75140f0f396663600895b3b88a766fab164b3c
ParthSingh2311 <91386914+ParthSingh2311@users.noreply.github.com>
2021-09-25 23:29:44 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,33 +1,33 @@
-makeCacheMatrix<-function(x=matrix()){
-  invert<-NULL
-  matrix_value<-function(y)
+makeCacheMatrix<-function(x=matrix()){ #The main function that will be used to input the values
+  invert<-NULL                         # setting the default value of the inverse of the matrix to null
+  matrix_value<-function(y)            # function to set the value of the matrix
   {
     x<<-y
     invert<<-NULL
   }
-  get<-function()
+  get<-function()                      # function to get the value of the matrix
   {
     x
   }
-  setInverse<-function(inverse){
+  setInverse<-function(inverse){       # function to set the value of the inverse of the matrix
     invert<<-inverse
     
   }
-  getInverse<-function()
+  getInverse<-function()               # function to fetch the value of the inverse of the matrix
   {
     invert
   }
   list(matrix_value=matrix_value, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
-cacheSolve<-function(x,...){
+cacheSolve<-function(x,...){          # function to solve, i.e. give the inverse of the matrix. 
   invert<-x$getInverse()
-  if(!is.null(invert)){
-    message("Getting the Data")
+  if(!is.null(invert)){               # condition to see if the value is already cached. If it is it will print the value along with a message. Otherwise, it would say NULL
+    message("Getting the Data from Cache")
     return(invert)
   }
   mat<-x$get()
-  invert<-solve(mat,...)
-  x$setInverse(invert)
+  invert<-solve(mat,...)             # function to get the inverse of the matrix stored in the variable, invert
+  x$setInverse(invert)               # sending the value of the inverse to the cache
   invert
 }
